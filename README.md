793eb5a939937fc2fb415b22a1ebb744c1695c66
m-a-hossain <adam.eve007@gmail.com>
2021-05-13 23:48:57 +0600

    part2 finish

--- README.md
+++ README.md
@@ -15,3 +15,7 @@
 3. **express.text()**url encoded as a text a=name
 4.  **express.urlencoded()**if a data send from a form 
 **content type must be "application/x-www-form-urlencoded"**
+5. **express.static()**
+  ..*if a folder or file to use as a static then go for bellow system ->
+  ..*make a folder in public and make a test folder in public and a home.html can call form url localhost/4000/test/
+6. **express.router()** will another part
--- index.js
+++ index.js
@@ -5,13 +5,26 @@ const express =require('express')
 const app = express()
 
 //when a request send from body content type 	"application/octet-stream" we will get a buffer object
-app.use(express.raw())
+//app.use(express.raw())
 
 //declaring express for using data json object from the body by postman or browser
 //and postman must declare the header content type is application/json
 
 // when we send content type 	"application/json"
 /* app.use(express.json()) */
+
+// when we send a text into the request 
+/* app.use(express.text()) */
+
+// when we send data from url encode form data we use express.urlencoded()
+/* app.use(express.urlencoded()) */
+
+// if a request in url for a static file generate or show in domein name/.... 
+/* app.use(express.static(`${__dirname}/public/`)) */
+//if i call a home.html page
+app.use(express.static(`${__dirname}/public`,{
+    index:'home.html'
+}))
 
 //set a get router
 app.get('/',(req,res)=>{
@@ -23,8 +36,8 @@ app.get('/',(req,res)=>{
 app.post('/',(req,res)=>{   
     // can access every object from post by .notaion for object access
     console.log(req.body.lastName)
-    console.log(req.body)
-    console.log(req.body.toString())
+    console.log(req.body)//a buffer will come out
+    //console.log(req.body.toString()) // get the buffer to a string
     res.send('this is home page with post request')
 })
 
